225976aaaad1bccf8e7ec1f87d28097491708420
GabeGyn <gblcr@discente.ufg.br>
2025-06-11 10:15:23 -0300

    Testando o Swagger

--- Program.cs
+++ Program.cs
@@ -2,8 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using Invent.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-var  MyCorsPolicy = "_MyCorsPolicy";
 
+var  MyCorsPolicy = "_MyCorsPolicy";
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: MyCorsPolicy, policy  =>
@@ -13,6 +13,7 @@ builder.Services.AddCors(options =>
         .AllowAnyMethod();
     });
 });
+
 // Add services to the container.
 builder.Services.AddControllers();
 
